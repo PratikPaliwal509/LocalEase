d76c759463e805bf5feab73bd6ac5f3e4f01d5a8
Prakhar Shrivastava <prakhars2202@gmail.com>
2025-07-26 10:46:34 +0530

    commit changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginSignup } from './login-signup/login-signup';
 import { LandingPage } from './landing-page/landing-page';
+import { BuyerDashboard } from './buyer-dashboard/buyer-dashboard';
+import { VendorDashboard } from './vendor-dashboard/vendor-dashboard';
+import { OrderTracking } from './order-tracking/order-tracking';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginSignup, LandingPage],
+  imports: [RouterOutlet, LoginSignup, LandingPage, BuyerDashboard, VendorDashboard, OrderTracking],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
